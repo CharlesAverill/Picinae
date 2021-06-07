84922cfd76177f10cc7c2539f67786b890adff70
Dakota Fisher <dafi7849@colorado.edu>
2021-06-07 23:53:54 +0000

    progress

--- strspn_arm_proofs.v
+++ strspn_arm_proofs.v
@@ -27,8 +27,8 @@ Print list.
 
 Fixpoint cstring m loc s :=
   match s with
-  | [] => m Ⓑ[loc] = 0
-  | x :: xs => x <> 0 /\ m Ⓑ[loc] = x /\ cstring m (loc⊕1) xs
+  | [] => m loc = 0
+  | x :: xs => x <> 0 /\ m loc = x /\ cstring m (loc⊕1) xs
   end.
 
 (* Inductive cstring : (addr -> N) -> addr -> list N -> Prop := *)
@@ -49,8 +49,8 @@ Definition strspn_post strloc acceptloc accept m (_ : exit) s :=
     (* s R_R1 = Ⓓacceptloc /\ *)
     s R_R0 = Ⓓanswer /\
     cstring m acceptloc accept /\
-    (forall n, n < answer -> In (m Ⓑ[strloc ⊕ n]) accept) /\
-    ~In (m Ⓑ[strloc ⊕ answer]) accept.
+    (forall n, n < answer -> In (m (strloc ⊕ n)) accept) /\
+    ~In (m (strloc ⊕ answer)) accept.
 
 Definition strspn_invs strloc acceptloc accept m a s :=
   (* let common n m := *)
@@ -62,17 +62,17 @@ Definition strspn_invs strloc acceptloc accept m a s :=
   match a with
   | 16 =>
     Some (s R_R0 = Ⓓstrloc /\ s R_R1 = Ⓓacceptloc /\ s V_MEM32 = Ⓜm /\
-          s R_R12 = Ⓓm Ⓑ[strloc])
+          s R_R12 = Ⓓ(m strloc))
   | 28 =>
     Some
       (exists strind,
           s R_R0 = Ⓓstrind /\
           s R_R1 = Ⓓacceptloc /\
-          s R_R4 = Ⓓ(m Ⓑ[acceptloc]) /\
-          s R_R12 = Ⓓ(m Ⓑ[strloc⊕strind]) /\
+          s R_R4 = Ⓓ(m acceptloc) /\
+          s R_R12 = Ⓓ(m (strloc⊕strind)) /\
           s R_LR = Ⓓ(strloc⊕strind) /\
           cstring m acceptloc accept /\
-          forall i, i < strind -> In (m Ⓑ[strloc⊕i]) accept)
+          forall i, i < strind -> In (m (strloc⊕i)) accept)
   | 60 =>
     Some
       (exists strind acceptind,
@@ -80,13 +80,13 @@ Definition strspn_invs strloc acceptloc accept m a s :=
           s R_R1 = Ⓓacceptloc /\
           s R_R2 = Ⓓ(acceptloc⊕acceptind) /\
           (* s R_R3 = Ⓓ(m Ⓑ[acceptloc⊕acceptind]) /\ *)
-          s R_R4 = Ⓓ(m Ⓑ[acceptloc]) /\
-          s R_R12 = Ⓓ(m Ⓑ[strloc⊕strind]) /\
+          s R_R4 = Ⓓ(m acceptloc) /\
+          s R_R12 = Ⓓ(m (strloc⊕strind)) /\
           s R_LR = Ⓓ(strloc⊕strind) /\
           cstring m acceptloc accept /\
-          (forall i, i < strind -> In (m Ⓑ[strloc⊕i]) accept) /\
+          (forall i, i < strind -> In (m (strloc⊕i)) accept) /\
           (forall i, i < acceptind ->
-                     m Ⓑ[strloc⊕strind] <> m Ⓑ[acceptloc⊕i]))
+                     m (strloc⊕strind) <> m (acceptloc⊕i)))
   | _ => None
   end.
 
@@ -100,14 +100,31 @@ Theorem un_d_ify x y : Ⓓx = Ⓓy -> x = y.
   reflexivity.
 Qed.
 
+Local Ltac un_d_ify :=
+  repeat
+    match goal with
+    | [ HX : Ⓓ_ = Ⓓ_ |- _ ] => apply un_d_ify in HX
+    end; subst.
+
+Theorem cstr_nonil m loc s: cstring m loc s -> ~In 0 s.
+Proof.
+  intros.
+  generalize dependent loc.
+  induction s; [tauto|].
+  intros.
+  simpl in *.
+  intuition.
+  eapply IHs; eassumption.
+Qed.
+
 Theorem mod_sub_eq w n m :
-  n < 2^w -> m < 2^w -> 2 ^ w + n ⊖ m = 0 -> n = m.
+  n < 2^w -> m < 2^w -> ((2 ^ w + n) - m) mod 2^w = 0 -> n = m.
 Admitted.
 
-Theorem substr_accept_addone m strloc strind accept :
-  In (m Ⓑ[ strloc ⊕ strind ]) accept ->
-  (forall n, n < strind -> In (m Ⓑ[ strloc ⊕ n ]) accept) ->
-  forall n, n < strind + 1 -> In (m Ⓑ[ strloc ⊕ n ]) accept.
+Theorem substr_accept_addone m strloc strind (accept : list N) :
+  In (m (strloc ⊕ strind)) accept ->
+  (forall n, n < strind -> In (m (strloc ⊕ n)) accept) ->
+  forall n, n < strind + 1 -> In (m (strloc ⊕ n)) accept.
 Proof.
   intros.
   rewrite N.add_1_r in H1.
@@ -116,9 +133,9 @@ Proof.
   destruct H1; subst; auto.
 Qed.
 
-Theorem substr_accept_mod m strloc strind accept :
-  (forall n, n < strind -> In (m Ⓑ[ strloc ⊕ n ]) accept) ->
-  forall n, n < strind mod 2^32 -> In (m Ⓑ[ strloc ⊕ n ]) accept.
+Theorem substr_accept_mod m strloc strind (accept : list N) :
+  (forall n, n < strind -> In (m (strloc ⊕ n)) accept) ->
+  forall n, n < strind mod 2^32 -> In (m (strloc ⊕ n)) accept.
 Proof.
   intros.
   apply H.
@@ -126,10 +143,10 @@ Proof.
   apply N_mod_le.
 Qed.
 
-Theorem substr_accept_xaddone m strloc strind accept :
-  In (m Ⓑ[ strloc ⊕ strind ]) accept ->
-  (forall n, n < strind -> In (m Ⓑ[ strloc ⊕ n ]) accept) ->
-  forall n, n < strind ⊕ 1 -> In (m Ⓑ[ strloc ⊕ n ]) accept.
+Theorem substr_accept_xaddone m strloc strind (accept : list N) :
+  In (m (strloc ⊕ strind)) accept ->
+  (forall n, n < strind -> In (m (strloc ⊕ n)) accept) ->
+  forall n, n < strind ⊕ 1 -> In (m (strloc ⊕ n)) accept.
 Proof.
   intros.
   eapply substr_accept_mod; [|apply H1].
@@ -141,7 +158,7 @@ Theorem strspn_partial_correctness_loop:
          (MDL0: models armtypctx s)
          (MEM0: s V_MEM32 = Ⓜm) (SP0: s R_SP = Ⓓsp)
          (ARGSTRING: s R_R0 = Ⓓstrloc) (ARGACCEPT: s R_R1 = Ⓓacceptloc)
-         (INITCHAR: s R_R12 = Ⓓm Ⓑ[strloc])
+         (INITCHAR: s R_R12 = Ⓓm (strloc))
          (ACCEPT: cstring m acceptloc accept)
          (RET: strspn_arm s (m Ⓓ[ sp ⊕ 4 ]) = None)
          (XP0: exec_prog fh strspn_arm 16 s n s' x),
@@ -191,7 +208,7 @@ Proof.
       exists answer.
       intuition; subst; intuition.
       apply Neqb_ok in BC.
-      destruct (m Ⓑ[_]) eqn:R in BC; [|discriminate].
+      destruct (m _) eqn:R in BC; [|discriminate].
       destruct accept; [assumption|].
       red in H4.
       intuition.
@@ -203,34 +220,26 @@ Proof.
       rewrite Hsv0 in *.
       rewrite Hsv1 in *.
       rewrite Hsv2 in *.
-      Local Ltac un_d_ify :=
-        repeat
-          match goal with
-          | [ HX : Ⓓ_ = Ⓓ_ |- _ ] => apply un_d_ify in HX
-          end; subst.
       un_d_ify.
       eexists.
       split; [reflexivity|].
       psimpl_goal.
       rewrite N.add_assoc.
       intuition.
-      assert (NG : In (m Ⓑ[ strloc ⊕ prestrind ]) accept); [|admit].
+      eapply substr_accept_xaddone; eauto.
       apply Neqb_ok in BC0.
       destruct (_ =? 0) eqn:HEq in BC0; [|discriminate].
       apply Neqb_ok in HEq.
       unfold welltyped_memory in WTM.
-      assert (WTM32 : forall a, m Ⓑ[ a ] < 2 ^ 32).
+      assert (WTM32 : forall a, m a < 2 ^ 32).
       {
-        simpl.
-        unfold getmem_little.
         intros.
-        rewrite N.lor_0_r.
         eapply N.lt_trans; eauto.
         reflexivity.
       }
       apply mod_sub_eq in HEq; [|apply WTM32|apply WTM32].
-      red in H4.
-      destruct accept; [rewrite H4 in BC;discriminate|].
+      destruct accept; simpl in H4; [rewrite H4 in BC;discriminate|].
+      simpl.
       left.
       rewrite HEq in H4.
       intuition.
@@ -253,31 +262,27 @@ Proof.
       apply Neqb_ok in BC0.
       destruct (_ =? _) eqn:HEq1 in BC0; [|discriminate].
       apply Neqb_ok in HEq1.
-      assert (WTM32 : forall a, m Ⓑ[ a ] < 2 ^ 32).
+      assert (WTM32 : forall a, m a < 2 ^ 32).
       {
-        simpl.
-        unfold getmem_little.
         intros.
-        rewrite N.lor_0_r.
         eapply N.lt_trans; eauto.
         reflexivity.
       }
       apply mod_sub_eq in HEq1; [|apply WTM32|apply WTM32].
-      assert (cstr_nonil: forall m loc s, cstring m loc s -> ~In 0 s).
-      {
-        intros.
-        generalize dependent loc.
-        induction s0; [tauto|].
-        intros.
-        simpl in H.
-        simpl.
-        intuition.
-        eapply IHs0; eassumption.
-      }
       split; [assumption|].
       split.
       {
-        admit.
+        apply substr_accept_xaddone; [|assumption].
+        rewrite N.eqb_neq in BC.
+        destruct (_ =? _) eqn:HEq2 in BC; [contradiction|].
+        rewrite N.eqb_neq in HEq2.
+        destruct accept; [contradiction|].
+        simpl in H4.
+        intuition.
+        subst.
+        simpl.
+        left.
+        assumption.
       }
       {
         rewrite N.add_mod_idemp_r by discriminate.
@@ -291,6 +296,7 @@ Proof.
           rewrite N.lor_0_r.
           reflexivity.
         }
+        rewrite HEq.
         eapply cstr_nonil.
         eassumption.
       }
@@ -306,24 +312,111 @@ Proof.
       rewrite N.add_0_r.
       rewrite N.mod_small by assumption.
       intuition.
-      simpl.
-      unfold getmem_little.
-      rewrite N.lor_0_r.
-      reflexivity.
     }
   }
   {
+    destruct PRE as [pstrind [pacceptind HPRE]].
+    intuition.
     repeat step.
     {
+      eexists.
+      split; [reflexivity|].
+      psimpl_goal.
+      rewrite N.add_assoc.
+      intuition.
+      eapply substr_accept_xaddone; [|eassumption|eassumption].
+      apply Neqb_ok in BC0.
+      destruct (_ =? 0) eqn:HEq1 in BC0; [|discriminate].
+      apply Neqb_ok in HEq1.
+      assert (WTM32 : forall a, m a < 2 ^ 32).
+      {
+        intros.
+        eapply N.lt_trans; eauto.
+        reflexivity.
+      }
+      apply mod_sub_eq in HEq1; auto.
+      rewrite <- HEq1.
+      assert (LEN : pacceptind < N.of_nat (length accept)) by admit.
+      Set Nested Proofs Allowed.
+      Theorem cstr_accept_after m loc accept ind :
+        loc < 2^32 ->
+        cstring m loc accept ->
+        ind < N.of_nat (length accept) ->
+        In (m (loc ⊕ ind)) accept.
+      Proof.
+        revert loc accept.
+        induction ind using N.peano_ind.
+        {
+          intros.
+          rewrite N.add_0_r,N.mod_small by assumption.
+          destruct accept; [discriminate|].
+          simpl in *.
+          intuition.
+        }
+        {
+          intros.
+          rewrite <- N.add_succ_comm.
+          destruct accept; [edestruct N.nlt_0_r; eassumption|].
+          simpl length in H1.
+          rewrite Nat2N.inj_succ in H1.
+          rewrite <- N.succ_lt_mono in H1.
+          simpl in H0.
+          rewrite <- N.add_1_r.
+          Search (_ + _ mod _).
+          rewrite <- N.add_mod_idemp_l; [|discriminate].
+          simpl.
+          right.
+          apply IHind; intuition.
+          Search (_ mod _ < _).
+          apply N.mod_upper_bound.
+          discriminate.
+        }
+      Qed.
+      Theorem cstr_accept_after' m loc accept ind :
+        loc < 2^32 ->
+        cstring m loc accept ->
+        (forall n, n < ind -> m (loc ⊕ n) <> 0) ->
+        m (loc ⊕ ind) <> 0 ->
+        In (m (loc ⊕ ind)) accept.
+      Proof.
+        revert loc accept.
+        induction ind using N.peano_ind.
+        {
+          intros.
+          rewrite N.add_0_r,N.mod_small in * by assumption.
+          destruct accept; [contradiction|].
+          simpl in *.
+          intuition.
+        }
+        {
+          intros.
+          rewrite <- N.add_succ_comm in *.
+          rewrite <- N.add_mod_idemp_l; [|discriminate].
+          admit.
+        }
+      Abort.
+      rewrite <- N.add_assoc.
+      apply cstr_accept_after; auto.
       admit.
     }
     {
+      unfold strspn_post.
+      eexists.
+      split; [reflexivity|].
       admit.
     }
     {
+      exists pstrind, (pacceptind⊕1).
+      psimpl_goal.
+      rewrite N.add_assoc.
+      intuition.
       admit.
     }
     {
+      unfold strspn_post.
+      repeat rewrite update_frame by discriminate.
+      exists pstrind.
+      intuition.
       admit.
     }
   }
