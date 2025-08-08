a23d35f960972495336b1b98d488134e607b1a9f
Charles Averill <charlesaverill20@gmail.com>
2025-08-08 12:19:26 -0400

    Add bubble sort swap measurements

--- timing/examples/data_structures/array/bubble_sort/binary/main.c
+++ timing/examples/data_structures/array/bubble_sort/binary/main.c
@@ -1,5 +1,25 @@
 #include "../../../timing_experiments.h"
 #include "bubble_sort.h"
+#include <string.h>
+
+#define MAX_LEN 100
+
+inline void swap(uint32_t* x, uint32_t* y) {
+    uint32_t temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void bubble_sort_theta_n2_measure_swaps(uint32_t* arr, uint32_t size) {
+    for (uint32_t i = 0; i < size; i++) {
+        for (uint32_t j = 0; j < size - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+		printf("(%u, %u) ", i, j);
+                swap(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
+}
 
 int main() {
     neorv32_rte_setup();
@@ -7,13 +27,20 @@ int main() {
 
     srand(42);
 
-    for (uint32_t len = 1; len <= 100; ++len) {
-        uint32_t array[100];
+    for (uint32_t len = 1; len <= MAX_LEN; ++len) {
+        uint32_t array[MAX_LEN];
         for (uint32_t i = 0; i < len; i++) {
             array[i] = rand();
         }
 
-	    START_TIMER;
+	// Get swaps
+	uint32_t dupe[MAX_LEN];
+	memcpy(dupe, array, len);
+	printf("Swaps: [");
+	bubble_sort_theta_n2_measure_swaps(dupe, len);
+	printf("]\n");
+
+	START_TIMER;
         bubble_sort_theta_n2(array, len);
         PRINT_TIMER;
 
