f791ab8ac16d7bf911717244c9694571a1d294e3
Charles Averill <charlesaverill20@gmail.com>
2025-09-20 01:20:57 -0500

    vListInsert: one difficult case left, some dubious cases that can probably be fixed in invariant set

--- timing/examples/FreeRTOS/list/vListInsert.v
+++ timing/examples/FreeRTOS/list/vListInsert.v
@@ -1,154 +1,182 @@
-Require Import RTOSDemo_NoAsserts_Clz.
-Require Import riscvTiming.
+Require Import RTOSDemo.
+Require Import RISCVTiming.
 Import RISCVNotations.
-Require Import timing_auto.
+Require Import TimingAutomation.
+Require Import Arith.
 
-(* Common for all timing proofs - facilitates automation *)
-Module vListInsertTime <: TimingModule.
-    Definition time_of_addr (s : store) (a : addr) : N :=
-        match neorv32_cycles_upper_bound s (RTOSDemo_NoAsserts_Clz a) with
-        | Some x => x | _ => 999 end.
+Module TimingProof (cpu: CPUTimingBehavior).
 
+Module Program_vListInsert <: ProgramInformation.
     Definition entry_addr : N := 0x800023f0.
 
     Definition exits (t:trace) : bool :=
-    match t with (Addr a,_)::_ => match a with
-    | 0x80002428 => true
-    | _ => false
-  end | _ => false end.
-End vListInsertTime.
-Module vListInsertAuto := TimingAutomation vListInsertTime.
-Import vListInsertTime vListInsertAuto.
+        match t with (Addr a,_)::_ => match a with
+        | 0x80002428 => true
+        | _ => false
+    end | _ => false end.
 
-(* https://github.com/FreeRTOS/FreeRTOS-Kernel/blob/main/include/list.h#L147 *)
-Definition xItemValue (mem : addr -> N) (a : addr) : N :=
-    mem Ⓓ[a].
-Definition pxNext (mem : addr -> N) (a : addr) : addr :=
-    mem Ⓓ[4 + a].
-Definition portMAX_DELAY : N :=
-    0 ⊖ 1.
+    Definition binary := RTOSDemo.
+End Program_vListInsert.
 
-(* Get the nth item of a linked list *)
-Fixpoint linked_list_access (mem : addr -> N) (a : addr) (n : nat) : addr :=
-    match n with
-    | O => a
-    | S n' => linked_list_access mem (pxNext mem a) n'
-    end.
+Module RISCVTiming := RISCVTiming cpu Program_vListInsert.
+Module vListInsert := RISCVTimingAutomation RISCVTiming.
+Import Program_vListInsert vListInsert.
 
-(* Postcondition *)
-Definition time_of_vListInsert (t : trace) (base_mem : addr -> N) (a0 a1 : N) :=
-    cycle_count_of_trace t =
-        4 + time_mem.
+Module p <: LinkedListParams.
+  Definition w := 32.
+  Definition e := LittleE.
+  Definition dw := 8.
+  Definition pw := 4.
+  Global Transparent w e dw pw.
+End p.
+Module LL := Theory_RISCV.LinkedLists p.
+Import LL.
+Ltac psimpl_hook ::= psimpl.
+Ltac gdep x := generalize dependent x.
+Global Ltac Zify.zify_pre_hook ::= idtac.
 
-Definition vListInsert_terminates (mem : addr -> N) 
-    (pxList : addr) (pxNewListItem : addr) : Prop :=
-    let xValueOfInsertion := xItemValue mem pxNewListItem in 
-    xValueOfInsertion = portMAX_DELAY \/
-    exists k, 
-      xItemValue mem (linked_list_access mem pxList k) > xValueOfInsertion.
+Definition portMAX_DELAY := 2^32 - 1.
 
-(* Invariants *)
-(*
-     Entrypoint       
-          |           
-          v           
-  Degeneracy Check    
-          |<---------+
-          v          |
-    Return Point     |
-          |          |
-          v          |
-+--->Loop Start      |
-|         |          |
-|         v          |
-+----Break Check     |
-          |          |
-          v          |
-      Loop End-------+
-*)
-Definition vListInsert_timing_invs (base_mem : addr -> N) (a0 : N) (a1 : N)
-    (t:trace) : option Prop :=
+Definition time_of_vListInsert (mem : memory)
+    (pxNewListItem : addr) (maximal_index : N) (t : trace) :=
+  cycle_count_of_trace t =
+    tlw + taddi + taddi +
+    (if mem Ⓓ[pxNewListItem] =? portMAX_DELAY then (
+      tfbne + tlw
+    ) else (
+      ttbne + (maximal_index - 1) * (
+        taddi + tlw + tlw + ttbgeu
+      ) + taddi + tlw + tlw + tfbgeu + tjal
+    )) + (
+      tlw + 4 * tsw + tlw + tsw + taddi + tsw + tjalr
+    ).
+
+Definition exists_maximal (mem : memory) (pxList pxNewListItem maximal : addr)
+    (max_val new_val : N) (max_dist : nat):=
+  node_distance mem pxList maximal max_dist /\
+  list_node_value mem maximal = Some max_val /\
+  max_val <= new_val /\
+  LLForall (fun m a => 
+    match list_node_value m a with
+    | None => True
+    | Some val => val <= max_val
+    end
+  ) mem pxList.
+
+Definition vListInsert_timing_invs (base_mem : memory) (len : nat) (max_val new_val : N)
+    (pxList pxNewListItem maximal : addr) (dist_to_maximal : nat) (t:trace) : option Prop :=
 match t with (Addr a, s) :: t' => match a with
-(* Entrypoint *)
-| 0x800023f0 => Some (s V_MEM32 = Ⓜbase_mem /\ s R_A0 = Ⓓa0 /\ s R_A1 = Ⓓa1 /\
-                      vListInsert_terminates base_mem a0 a1 /\
-                        cycle_count_of_trace t' = 0)
-(* Degeneracy Check *)
-| 0x800023fc => Some (exists mem, s V_MEM32 = Ⓜmem /\ 
-                        s R_A0 = Ⓓa0 /\ s R_A1 = Ⓓa1 /\
-                        vListInsert_terminates mem a0 a1 /\
-                        cycle_count_of_trace t' = 4 + time_mem)
-(* Return Point *)
-| 0x80002428 => Some (time_of_vListInsert t base_mem a0 a1)
-(* Loop Start *)
-| 0x8000242c => Some (exists k, cycle_count_of_trace t' = 
-                        4 + time_mem + time_branch + 
-                        k * (2 + 2 * time_mem + time_branch)
-                     )
+| 0x800023f0 => Some (
+    exists_maximal (s V_MEM32) pxList pxNewListItem maximal
+      max_val new_val dist_to_maximal /\
+    s R_A0 = pxList /\ s R_A1 = pxNewListItem /\
+    s V_MEM32 = base_mem /\
+    pxList <> NULL /\
+    node_distance (s V_MEM32) pxList NULL len /\
+    cycle_count_of_trace t' = 0
+  )
+| 0x8000242c => Some (exists ctr,
+    exists_maximal (s V_MEM32) pxList pxNewListItem maximal
+      max_val new_val dist_to_maximal /\
+    pxList <> NULL /\
+    s V_MEM32 = base_mem /\
+    (base_mem Ⓓ[pxNewListItem] =? portMAX_DELAY) = false /\
+    node_distance (s V_MEM32) pxList NULL len /\
+    node_distance (s V_MEM32) pxList ((s V_MEM32) Ⓓ[s R_A4]) ctr /\
+    cycle_count_of_trace t' =
+      tlw + taddi + taddi +
+      ttbne + (N.of_nat ctr - 1) * (
+        taddi + tlw + tlw + ttbgeu
+      )
+  )
+| 0x80002428 => Some (time_of_vListInsert base_mem
+                    pxNewListItem (N.of_nat dist_to_maximal) t)
 | _ => None end | _ => None end.
 
-(* Lift the program *)
-Definition lifted_vListInsert : program :=
-    lift_riscv RTOSDemo_NoAsserts_Clz.
+Lemma le_cases : forall n m,
+    (n <= m -> n < m \/ n = m)%nat.
+Proof. lia. Qed.
 
 Theorem vListInsert_timing:
-  forall s t s' x' base_mem a0 a1
+  forall s t s' x' base_mem len pxList pxNewListItem maximal max_val
+            dist_to_maximal new_val
+         (* boilerplate *)
          (ENTRY: startof t (x',s') = (Addr entry_addr, s))
          (MDL: models rvtypctx s)
-         (MEM: s V_MEM32 = Ⓜbase_mem)
-         (A0: s R_A0 = Ⓓa0)
-         (A1: s R_A1 = Ⓓa1)
-         (TERMINATES: vListInsert_terminates base_mem a0 a1),
-  satisfies_all 
-    lifted_vListInsert
-    (vListInsert_timing_invs base_mem a0 a1)
+         (* bindings *)
+         (MEM: s V_MEM32 = base_mem)
+         (A0: s R_A0 = pxList)
+         (A1: s R_A1 = pxNewListItem)
+         (* pxList is non-null (guaranteed by vListInitialise) *)
+         (PXL_NN: pxList <> NULL)
+         (* list length is finite *)
+         (LEN: node_distance (s V_MEM32) pxList NULL len)
+         (NLI_VAL: list_node_value base_mem pxNewListItem = Some new_val)
+         (* there exists a maximal node in the list with a value
+            less than or equal to that of pxNewListItem *)
+         (MAX: exists_maximal base_mem pxList pxNewListItem maximal max_val new_val dist_to_maximal),
+  satisfies_all
+    lifted_prog
+    (vListInsert_timing_invs base_mem len max_val new_val pxList pxNewListItem maximal dist_to_maximal)
     exits
   ((x',s')::t).
 Proof using.
-    (* Specialize some automation tactics for our purposes *)
-    Local Ltac step := time rv_step.
-
-    (* Setup *)
     intros.
     apply prove_invs.
-        simpl. rewrite ENTRY. unfold entry_addr. now step.
 
-        intros.
-        eapply startof_prefix in ENTRY; try eassumption.
-        eapply preservation_exec_prog in MDL; 
-            try eassumption; [idtac|apply lift_riscv_welltyped].
-        clear - ENTRY PRE MDL. rename t1 into t. rename s1 into s'.
+    Local Ltac step := tstep r5_step.
+    simpl. rewrite ENTRY. unfold entry_addr. step.
+    rewrite MEM.
+    split. assumption. repeat split; auto.
+    now rewrite <- MEM.
 
-    (* Proof start *)
+    intros.
+    eapply startof_prefix in ENTRY; try eassumption.
+    eapply preservation_exec_prog in MDL;
+        try eassumption; [idtac|apply lift_riscv_welltyped].
+    clear - ENTRY PRE MDL. rename t1 into t. rename s1 into s'.
+
     destruct_inv 32 PRE.
 
-    (* Entrypoint *)
-    destruct PRE as (MEM & A0 & A1 & TERMINATES & Cycles).
-    repeat step.
-    (* 0x800023fc *) {
-        handle_ex. hammer. find_rewrites. unfold time_mem. lia.
-    }
-
-    destruct PRE as (mem & MEM & A0 & A1 & TERMINATES & Cycles).
-    step.
-    (* 0x8000242c - loop entry *) {
-        exists 0. hammer. find_rewrites. unfold time_mem, time_branch.
+    - destruct PRE as (EMax & A0 & A1 & MEM & PXL_NN & Len & Cycles).
+      repeat step.
+      -- (* 0x8000242c invariant *)
+        exists 1%nat. rewrite MEM in *. split. assumption.
+        repeat split; auto.
+        now apply Bool.negb_true_iff in BC. 
+        apply node_distance_next_S_len with (dst := pxList).
+          unfold list_node_next. destruct pxList.
+            contradiction.
+            reflexivity.
+          eapply distance_null_imp_well_formed; eassumption.
+          apply Dst0.
+          hammer.
+      -- (* 0x80002428 postcondition *)
+        unfold portMAX_DELAY.
+        hammer.
+    - destruct PRE as (ctr & EMax & PXL_NN & MEM & NLI_VALID & Len & DstCurr & 
+          Cycles).
+      repeat step.
+      -- (* 0x80002428 postcondition *)
+        rewrite NLI_VALID.
+        hammer. replace ctr with dist_to_maximal.
         lia.
-    }
-
-    (* 0x80002428 - postcondition after degeneracy check fails *)
-    admit.
-
-    (* 0x8000242c - loop iterate *)
-    repeat step.
-
-    (* 0x80002428 - postcondition after loop exits *)
-    admit.
-
-    (* 0x8000242c - loop invariant after iteration *)
-    destruct PRE as (k & Cycles).
-    exists k. hammer. find_rewrites. apply Bool.negb_true_iff in BC.
-    rewrite BC. psimpl. unfold time_mem, time_branch. psimpl. 
-
-Qed.
-
+        destruct EMax, H0, H1.
+        enough (s' V_MEM32 Ⓓ[s' R_A4] = maximal).
+          rewrite H3 in *.
+          eapply node_distance_uniq; eassumption.
+        (* If we can show that we have reached the maximal 
+           list item, this is proven
+        *)
+        admit.
+      -- exists (S ctr). rewrite MEM in *.
+         split. assumption. repeat split; auto.
+         eapply node_distance_next_S_len; eauto.
+         unfold list_node_next.
+         admit.
+         eapply distance_null_imp_well_formed; eassumption.
+         replace (N.of_nat (S ctr) - 1) with (N.of_nat ctr) by lia.
+         hammer. replace (N.of_nat ctr) with (1 + N.of_nat ctr - 1) at 2 by lia.
+         rewrite <- N.add_sub_assoc. lia.
+         admit.
+Admitted.
