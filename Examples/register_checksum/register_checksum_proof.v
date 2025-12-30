dd9b460c5387101f790ff493aae76af421bd6686
Charles Averill <charlesaverill20@gmail.com>
2025-12-30 16:29:33 -0600

    Add register_checksum skip proof, memory corruption injection hangs on step and typecheck

--- Examples/register_checksum/register_checksum_proof.v
+++ Examples/register_checksum/register_checksum_proof.v
@@ -12,7 +12,7 @@ Open Scope N_scope.
 Section Invariants.
     Variable    a1 a2 a3 a4 a5 a6 a7
              s0 s1 s2 s3 s4 s5 s6 s7 s8 s9 s10 s11
-             checksum : N.
+             checksum fc : N.
 
     Definition postcondition : Prop :=
         (     a1 ⊕ a2 ⊕ a3 ⊕ a4 ⊕ a5 ⊕ a6 ⊕ a7 ⊕
@@ -104,16 +104,33 @@ Section FaultTolerantInvariants.
                 checksum : N.
 
     Definition register_checksum_faulty : program :=
-        inject_memory_corruption (inject_skip register_checksum).
+        inject_skip (register_checksum).
 
-    Definition invs' :=
-            invs a1 a2 a3 a4 a5 a6 a7
-                 s0 s1 s2 s3 s4 s5 s6 s7 s8 s9 s10 s11
-                 checksum.
+    Definition invs' T (Inv Post: inv_type T) (NoInv:T) (s:store) (a:addr) : T :=
+        let regs := regs a1 a2 a3 a4 a5 a6 a7
+                s0 s1 s2 s3 s4 s5 s6 s7 s8 s9 s10 s11
+                checksum in
+        let postcondition := postcondition a1 a2 a3 a4 a5 a6 a7
+                s0 s1 s2 s3 s4 s5 s6 s7 s8 s9 s10 s11
+                checksum in
+        match a with
+        | 0x0 => Inv 0 (regs s /\ s V_FC = 1)
+        (* Goal space cinch *)
+        | 0x98 => Inv 0 (regs s /\ s V_FC <= 1 /\
+                         s R_T1 = a1 ⊕ a2 ⊕ a3 ⊕ a4 ⊕ a5 ⊕ a6 ⊕ a7)
+        | 0x1c0 | 0x1c4 => Post 0 (~ postcondition)
+        | 0x1c8 | 0x1cc => Post 0 (postcondition)
+        | _ => NoInv
+        end.
 
     Definition exits0' := make_exits 0 register_checksum_faulty invs'.
     Definition invs0' := make_invs 0 register_checksum_faulty invs'.
 End FaultTolerantInvariants.
+
+Lemma register_checksum_faulty_wt:
+    welltyped_prog rvtypctx register_checksum_faulty.
+Proof.
+Admitted.
 
 (* Proof in single-fault context *)
 Theorem register_checksum_correctness_faulty:
@@ -124,8 +141,9 @@ Theorem register_checksum_correctness_faulty:
          (MDL: models rvtypctx s)
          (REGS: regs a1 a2 a3 a4 a5 a6 a7
                      s0 s1 s2 s3 s4 s5 s6 s7 s8 s9 s10 s11
-                     checksum s),
-  satisfies_all register_checksum 
+                     checksum s)
+         (FC: s V_FC = 1),
+  satisfies_all register_checksum_faulty 
         (invs0' a1 a2 a3 a4 a5 a6 a7
                 s0 s1 s2 s3 s4 s5 s6 s7 s8 s9 s10 s11
                 checksum)
@@ -133,22 +151,85 @@ Theorem register_checksum_correctness_faulty:
                  s0 s1 s2 s3 s4 s5 s6 s7 s8 s9 s10 s11
                  checksum) (xs'::t).
 Proof using.
+    Local Ltac step ::= 
+        match goal with
+        | [s' : store, FC : ?s' V_FC <= 1 |- _] =>
+            let H := fresh "H" in
+            assert (s' V_FC = 0 \/ s' V_FC = 1) as H by lia;
+            clear FC; destruct H
+        | _ => idtac
+        end;
+        r5_step;
+        repeat match goal with
+        | [n : N, BC : ?n mod 2 = _ |- _] => clear BC n
+        | [s' : store, n : N, 
+            BC : (if 0 <? ?s' V_FC then ?n mod 2 else 0) = _ |- _] => clear BC n
+        | [H: ?x = ?x |- _] => clear H
+        end;
+        try discriminate.
     intros. apply prove_invs.
 
-    simpl. rewrite ENTRY. step. now unfold regs in REGS.
+    simpl. rewrite ENTRY. step.
+        split; [now unfold regs in REGS|assumption].
 
     intros.
     eapply startof_prefix in ENTRY; try eassumption.
-    eapply preservation_exec_prog in MDL; try (eassumption || apply lift_riscv_welltyped).
+    eapply preservation_exec_prog in MDL; try (eassumption || apply register_checksum_faulty_wt).
     clear - PRE MDL. rename t1 into t. rename s12 into s. rename a0 into a.
 
     destruct_inv 32 PRE.
 
     destruct PRE as
-        (CHK & A1 & A2 & A3 & A4 & A5 & A6 & A7 &
-          S0 & S1 & S2 & S3 & S4 & S5 & S6 & S7 & S8 & S9 & S10 & S11).
+        ((CHK & A1 & A2 & A3 & A4 & A5 & A6 & A7 &
+          S0 & S1 & S2 & S3 & S4 & S5 & S6 & S7 & S8 & S9 & S10 & S11) & 
+          FC). {
+    replace s with (update s V_FC 1) by
+        now erewrite store_upd_eq.
 
-    repeat step; try lia;
-        intro Contra; unfold postcondition in Contra;
-        psimpl in Contra; lia.
+    repeat (match goal with
+            | [|- context[Addr ?x]] =>
+                idtac x "/" 460
+            end; step; [|
+        time (repeat step; idtac "Solving invariant";
+        repeat split; psimpl; auto; lia)]).
+
+    split. unfold regs; psimpl;
+        repeat split; auto.
+    split; reflexivity.
+    }
+
+    destruct PRE as
+        ((CHK & A1 & A2 & A3 & A4 & A5 & A6 & A7 &
+          S0 & S1 & S2 & S3 & S4 & S5 & S6 & S7 & S8 & S9 & S10 & S11) & 
+          FC & T1).
+
+    Ltac solve_post :=
+        match goal with
+        | [BC: _, BC0: _ |- _ -> False] =>
+            let Contra := fresh "Contra" in
+            intro Contra; unfold postcondition in Contra;
+            psimpl in Contra; rewrite Contra in BC0;
+            rewrite N.eqb_refl in BC0; discriminate
+        | [BC: (_ =? _) = true |- _] =>
+            apply N.eqb_eq in BC; now rewrite BC
+        end.
+
+    repeat (match goal with
+            | [|- context[Addr ?x]] =>
+                idtac x "/" 460
+            end; step; match goal with 
+            | [|- context[update _ V_FC 0]] => 
+                idtac "Faulted, stepping to postcondition...";
+                time (repeat time step; idtac "Solving postcondition";
+                    solve_post)
+            | _ => idtac
+            end).
+
+    apply N.eqb_eq in BC0; now rewrite BC0.
+    intro Contra; unfold postcondition in Contra;
+        psimpl in Contra; rewrite Contra in BC1;
+        rewrite N.eqb_refl in BC1; discriminate.
+    intro Contra; unfold postcondition in Contra;
+        psimpl in Contra; rewrite Contra in BC0;
+        rewrite N.eqb_refl in BC0; discriminate.
 Qed.
